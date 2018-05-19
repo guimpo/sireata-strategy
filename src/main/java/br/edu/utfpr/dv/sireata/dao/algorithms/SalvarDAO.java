3c6f146e9b531dedaf1fb11c1c29d8a12eab0052
Paulo Nakaima <nakaima.prk@gmail.com>
2018-05-19 19:18:22 -0300

    Criada interface salvarDAO e implementação de anexoSalvarDAO

---
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/algorithms/SalvarDAO.java
@@ -0,0 +1,8 @@
+package br.edu.utfpr.dv.sireata.dao.algorithms;
+
+import br.edu.utfpr.dv.sireata.dao.DAOEntity;
+
+public interface SalvarDAO {
+    
+    public int salvar (DAOEntity e) throws Exception;
+}
