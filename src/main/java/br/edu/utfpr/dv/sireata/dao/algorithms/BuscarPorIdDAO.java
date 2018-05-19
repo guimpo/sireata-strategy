98eb0c704b098b39511cfb85df2cacbfd5ccfb86
Paulo Nakaima <nakaima.prk@gmail.com>
2018-05-19 20:36:37 -0300

    Problema do plugin git do netbeans

--- src/main/java/br/edu/utfpr/dv/sireata/dao/algorithms/BuscarPorIdDAO.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.dv.sireata.dao.algorithms;
-
-import br.edu.utfpr.dv.sireata.dao.DAOEntity;
-
-public interface BuscarPorIdDAO {
-    
-    public DAOEntity buscarPorId(int id) throws Exception;
-}
